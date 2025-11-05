8c37c4c0a0b2df337de82ec04fd96f4410ac9910
jhincita <sadwaifv@gmail.com>
2025-11-05 19:34:28 -0300

    agregados iconos navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Home from "./pages/Home";
+import HomeImg from "./assets/navbar/home.svg"
+
 import Login from "./pages/Login";
+import LoginImg from "./assets/navbar/login.svg"
 import Menu from "./pages/Menu";
+import MenuImg from "./assets/navbar/menu.svg"
 import About from "./pages/About";
+import AboutImg from "./assets/navbar/about.svg"
 import Contact from "./pages/Contact";
+import ContactImg from "./assets/navbar/contact.svg"
 import Blogs from "./pages/Blogs";
+import BlogsImg from "./assets/navbar/blogs.svg"
 import CartPage from "./pages/CartPage.jsx";
+import CartImg from "./assets/navbar/cart.svg"
+import BuildAPizza from "./pages/BuildAPizza.jsx";
+import BuildAPizzaImg from "./assets/navbar/buildAPizza.svg"
+
 import AdminPanel from "./pages/AdminPanel";
+
 import Juego from './pages/Juego.jsx';
 import Window from "./components/Window";
+
+
 import './App.css';
 
 function ProtectedAdminRoute({ children }) {
@@ -27,21 +41,42 @@ function MainPage({ User, setUser, cart, setCart, handleLoginSuccess }) {
     const [openWindow, setOpenWindow] = useState(null);
 
     const pages = {
-        home: <Home User={User} setUser={setUser} />,
-        login: <Login 
-            User={User} 
-            setUser={setUser} 
-            onLoginSuccess={handleLoginSuccess}
-            onClose={() => setOpenWindow(null)}
-        />,
-        menu: <Menu cart={cart} setCart={setCart} />,
-        about: <About />,
-        contact: <Contact />,
-        blogs: <Blogs />,
-        cart: <CartPage cart={cart} setCart={setCart} />,
-        juego: <Juego />   
-    };
+        Home:{
+            img: HomeImg,
+            component: <Home User={User} setUser={setUser} />
 
+        } ,
+        login: {
+            img: LoginImg,
+            component: <Login User={User} setUser={setUser} onLoginSuccess={handleLoginSuccess} />
+        },
+        menu: {
+            img: MenuImg,
+            component: <Menu cart={cart} setCart={setCart} />
+        } ,
+        about:{
+            img: AboutImg,
+            component: <About />
+        } ,
+        contact: {
+            img: ContactImg,
+            component: <Contact />
+        },
+        blogs: {
+            img: BlogsImg,
+            component: <Blogs />
+        },
+        cart: {
+            img: CartImg,
+            component: <CartPage cart={cart} setCart={setCart} />
+
+        },
+        personalizar: {
+            img: BuildAPizzaImg,
+            component: <BuildAPizza setUser={setUser}/>
+
+        }
+    };
     return (
         <div className="page-wrapper">
             <header className="header">
@@ -55,24 +90,34 @@ function MainPage({ User, setUser, cart, setCart, handleLoginSuccess }) {
             <nav className="window">
                 <p className="window-title">Nav</p>
                 <div className="container">
-                    {Object.keys(pages).map((key) => (
+                    {Object.entries(pages).map(([key, { img }]) => (
                         <button
                             key={key}
-                            className="button"
+                            className="nav-button"
                             onClick={() => setOpenWindow(key)}
                         >
-                            {key === 'game' ? 'JUEGO' : key.toUpperCase()}
+                            <img
+                                src={img}
+                                alt={key}
+                                className="nav-icon"
+                            />
+                            <span className="nav-label">
+                    {key === 'personaliza'
+                        ? 'PERSONALIZA'
+                        : key.toUpperCase()}
+                </span>
                         </button>
                     ))}
                 </div>
             </nav>
+
 
             <Window
                 title={openWindow ? openWindow.toUpperCase() : ""}
                 isOpen={!!openWindow}
                 onClose={() => setOpenWindow(null)}
             >
-                {openWindow && pages[openWindow]}
+                {openWindow && pages[openWindow].component}
             </Window>
         </div>
     );
